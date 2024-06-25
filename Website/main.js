eddc01e543dbacf84d527f8f5d440a9a7a150d26
Akın <kekikcilerakin@gmail.com>
2024-06-26 02:12:50 +0300

    removed unused website

--- Website/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $.get("http://localhost:3000/items", function (data) {
-    console.log("data", data);
-    // data.forEach(function (item) {
-    //   $("#items-list").append(`<li>${item.name} - ${item.cost} - ${item.imageUrl}</li>`);
-    // });
-  });
-});
